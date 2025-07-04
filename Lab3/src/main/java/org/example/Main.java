ab6720580bffcce8b1e44716674b3a473aa52875
Ralph Anthony Rafael <ralphanthony.rafael@gcash.com>
2025-07-04 10:53:23 +0800

    Uploaded BankSystem

--- Lab3/src/main/java/org/example/Main.java
+++ Lab3/src/main/java/org/example/Main.java
@@ -1,17 +1,180 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    private static ArrayList<BankAccount> bankAccounts = new ArrayList<>();
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        boolean hasNotExit = true;
+
+        while(hasNotExit!=false) {
+            System.out.println("=== Bank Menu ===");
+            System.out.println("1. Create Account");
+            System.out.println("2. View All Accounts");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Deposit");
+            System.out.println("5. Withdraw");
+            System.out.println("6. Exit");
+
+            System.out.printf("Enter choice (1-5): ");
+            int currentChoice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (currentChoice) {
+                case 1:
+                    createAccount();
+                    break;
+                case 2:
+                    viewAllAccounts();
+                    break;
+                case 3:
+                    checkBalance();
+                    break;
+                case 4:
+                    depositMoney();
+                    break;
+                case 5:
+                    withdrawMoney();
+                    break;
+                case 6:
+                    hasNotExit=false;
+                    System.out.println("----------------------------------------");
+                    System.out.println("Thank you for using our banking service!");
+                    System.out.println("----------------------------------------");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid option");
+            }
+
+            System.out.println("\n");
+            System.out.printf("Would you like to return to the menu? (yes/no): ");
+
+            String response2 = scanner.nextLine();
+            if (response2.equals("yes")) {
+                continue;
+            }
+
+            else if (response2.equals("no")) {
+
+                System.out.println("----------------------------------------");
+                System.out.println("Thank you for using our banking service!");
+                System.out.println("----------------------------------------");
+                return;
+            }
+        }
+        System.out.println("----------------------------------------");
+        System.out.println("Thank you for using our banking service!");
+        System.out.println("----------------------------------------");
+    }
+
+    private static void createAccount() {
+        System.out.printf("Enter Account Number: ");
+        int accountNumber = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.printf("Enter Holder Name: ");
+        String name = scanner.nextLine();
+
+        System.out.printf("Initial deposit? (yes/no): ");
+        String response = scanner.nextLine();
+        double deposit = 0;
+
+        if (response.equals("yes")) {
+            System.out.printf("Enter initial deposit money: ");
+            deposit = scanner.nextDouble();
+            scanner.nextLine();
+
+            if (deposit < 0) {
+                System.out.println("Deposit has to be non-negative.");
+                return;
+            }
+        }
+
+        BankAccount newAccount = new BankAccount(accountNumber, name, deposit);
+        bankAccounts.add(newAccount);
+        System.out.println("Account created successfully!");
+
+    }
+
+    private static void viewAllAccounts() {
+        for (BankAccount bankAccount: bankAccounts) {
+            System.out.println("----------------------");
+            bankAccount.displayInfo();
+            System.out.println("----------------------");
         }
     }
+
+    private static BankAccount findAccount(int number) {
+        for (BankAccount bankAccount : bankAccounts) {
+            if(bankAccount.getAccountNumber() == number) {
+                return bankAccount;
+            }
+        }
+        return null;
+    }
+
+    private static void checkBalance() {
+        System.out.printf("Enter account number: ");
+        int accountNumber = scanner.nextInt();
+        scanner.nextLine();
+        BankAccount bankAccount = findAccount(accountNumber);
+
+        if(bankAccount != null) {
+            System.out.println("----------------------");
+            System.out.println("Balance: " + bankAccount.getBalance());
+            System.out.println("----------------------");
+        }
+
+        else {
+            System.out.println("----------------------");
+            System.out.println("Account does not exist.");
+            System.out.println("----------------------");
+        }
+    }
+
+    private static void depositMoney() {
+        System.out.printf("Enter account number: ");
+        int accountNumber = scanner.nextInt();
+        scanner.nextLine();
+        BankAccount bankAccount = findAccount((accountNumber));
+
+        if (bankAccount != null) {
+            System.out.printf("Enter deposit amount: ");
+            double amount = scanner.nextDouble();
+            scanner.nextLine();
+
+            bankAccount.depositMoney(amount);
+        }
+        else {
+            System.out.println("----------------------");
+            System.out.println("Account not found.");
+            System.out.println("----------------------");
+        }
+    }
+
+    private static void withdrawMoney() {
+        System.out.printf("Enter account number: ");
+        int accountNumber = scanner.nextInt();
+        scanner.nextLine();
+        BankAccount bankAccount = findAccount((accountNumber));
+
+        if (bankAccount != null) {
+            System.out.printf("Enter withdrawal amount: ");
+            double amount = scanner.nextDouble();
+            scanner.nextLine();
+            System.out.println("----------------------");
+            bankAccount.withdrawMoney(amount);
+            System.out.println("----------------------");
+        }
+        else {
+            System.out.println("----------------------");
+            System.out.println("Account not found.");
+            System.out.println("----------------------");
+        }
+    }
+
 }
