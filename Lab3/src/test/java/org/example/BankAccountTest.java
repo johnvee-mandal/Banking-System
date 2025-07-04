cc24d0cd98295be9bf98bea3cfc0ff14b91e5420
Tyrell Teppang <tyrellmarcus.teppang@gcash.com>
2025-07-04 14:28:53 +0800

    j unit testing methods are complete

--- Lab3/src/test/java/org/example/BankAccountTest.java
+++ Lab3/src/test/java/org/example/BankAccountTest.java
@@ -3,6 +3,8 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class BankAccountTest {
 
     private BankAccount testSavingsAccount;
@@ -15,18 +17,66 @@ class BankAccountTest {
     }
 
     @Test
-    void getAccountNumber() {
+    void testBankAccountCreation(){
+        BankAccount accountCreationTest_Savings = new BankAccount("savings", 3, "Jack Doe", 1000);
+        BankAccount accountCreationTest_Checking = new BankAccount("checking", 3, "Jamarcus Doe", 1000);
+
+        assertNotNull(accountCreationTest_Savings);
+        assertNotNull(accountCreationTest_Checking);
     }
 
     @Test
-    void depositMoney() {
+    void testValidWithdrawAmount() {
+
+        testSavingsAccount.withdrawMoney(500);
+        assertEquals(500,testSavingsAccount.getBalance(), 0.001);
+
+        testCheckingAccount.withdrawMoney(500);
+        assertEquals(500,testCheckingAccount.getBalance(), 0.001);
+
     }
 
     @Test
-    void withdrawMoney() {
+    void testInvalidWithdrawAmount() {
+
+        testSavingsAccount.withdrawMoney(-500);
+        assertEquals(testSavingsAccount.getInitialBalance(),testSavingsAccount.getBalance(), 0.001);
+
+        testCheckingAccount.withdrawMoney(-500);
+        assertEquals(testCheckingAccount.getInitialBalance(),testCheckingAccount.getBalance(), 0.001);
+
     }
 
     @Test
-    void addInterest() {
+    void testValidDepositAmount() {
+
+        testSavingsAccount.depositMoney(500);
+        assertEquals(1500,testSavingsAccount.getBalance(), 0.001);
+
+        testCheckingAccount.depositMoney(500);
+        assertEquals(1500,testCheckingAccount.getBalance(), 0.001);
+
+    }
+
+    @Test
+    void testInvalidDepositAmount() {
+
+        testSavingsAccount.depositMoney(-500);
+        assertEquals(testSavingsAccount.getInitialBalance(),testSavingsAccount.getBalance(), 0.001);
+
+        testCheckingAccount.depositMoney(-500);
+        assertEquals(testCheckingAccount.getInitialBalance(),testCheckingAccount.getBalance(), 0.001);
+
+    }
+
+    @Test
+    void testInterestCalculations() {
+
+        testSavingsAccount.addInterest();
+        assertEquals((testSavingsAccount.getInitialBalance() * 1.06),testSavingsAccount.getBalance(), 0.001);
+
+        testCheckingAccount.addInterest();
+        assertEquals((testCheckingAccount.getInitialBalance() * 1.01),testCheckingAccount.getBalance(), 0.001);
+
     }
 }
